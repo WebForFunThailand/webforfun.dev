c3b605d55b599102fb553face72b65eb5196db9d
tanakrid chanburi <tanakrid.c@ku.th>
2020-03-14 22:35:43 +0700

    add modal in gallery

---
+++ src/sections/gallery/Modal.js
@@ -0,0 +1,77 @@
+import React, {useState} from "react"
+import styled from "styled-components"
+
+import { Container, Heading } from "../../common/components"
+import { colors, fonts } from "../../common/style"
+
+const ModalBox = styled.div`
+    display: block; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    // background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+`
+
+const ModalContent = styled.img`
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+
+    src: ${props => props.src};
+`
+
+const Image = styled.img`
+    border-radius: 5px;
+    transition: 0.3s;
+    width: 100%;
+    height: 200px;
+    src: ${props => props.src};
+    display: none;
+
+    :hover {
+        opacity: 0.7;
+        cursor: pointer;
+    }
+`
+
+const Close = styled.span`
+    position: absolute;
+    top: 10%;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+
+    :hover, :focus{
+        color: #bbb;
+        text-decoration: none;
+        cursor: pointer;
+    }
+`
+
+export default function(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div>  
+            <Image src={props.src} onClick={() => setIsOpen(!isOpen)}/>
+            {isOpen && 
+                <ModalBox id="myModal">
+                    <Close onClick={() => setIsOpen(!isOpen)}>&times;</Close>
+                    <ModalContent id="img01" src={props.src}/>
+                </ModalBox>
+            }
+        </div>
+    )
+}
--- src/sections/gallery/index.js
+++ src/sections/gallery/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 import { Container, Heading, Center} from "../../common/components"
 import { colors, fonts, media } from "../../common/style"
 
-// import Modal from './modal'
+import Modal from './modal'
 
 const Section = styled.div`
   padding: 50px 0;
@@ -30,17 +30,14 @@ export default function() {
         <br/>
 
         <Grid>
-          {/* <Modal/>       */}
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
-
+          <Modal src="https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg"/>      
+          <Modal src="https://cdn.pixabay.com/photo/2018/12/21/14/26/view-3887981_960_720.jpg"/>      
+          <Modal src="https://image.shutterstock.com/image-photo/view-sea-houses-on-hillsides-600w-785350507.jpg"/>      
+          <Modal src="https://pix10.agoda.net/hotelImages/443/443651/443651_16090818170046261601.jpg?s=1024x768"/>      
+          <Modal src="https://www.tripgether.com/photos/shares/Pasu/5%20Mae%20salong/1015.jpg"/>      
+          <Modal src="https://articlekey.com/wp-content/uploads/2018/01/1.jpg"/>      
+          <Modal src="https://www.wangnokkaew.com/img/slide_new%20(2).jpg"/>      
+          <Modal src="https://www.chillpainai.com/src/wewakeup/chillwriter/uploads/2016-09-20%2015:08:46_Goodview12.jpg"/>      
         </Grid>
         <br/>
       </Container>
