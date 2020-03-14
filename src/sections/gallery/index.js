8c838ec0d99a2e1f007469164fc9f5c7f3c9f6fa
tanakrid chanburi <tanakrid.c@ku.th>
2020-03-14 21:44:24 +0700

    gallery no modal

--- src/sections/gallery/index.js
+++ src/sections/gallery/index.js
@@ -1,19 +1,49 @@
 import React from "react"
 import styled from "styled-components"
 
+import { Container, Heading, Center} from "../../common/components"
+import { colors, fonts, media } from "../../common/style"
+
+// import Modal from './modal'
+
 const Section = styled.div`
-  height: 100vh;
-  background: #ccc;
-  
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  padding: 50px 0;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+
+  ${media.desktop`
+    grid-template-columns: repeat(2, 1fr);
+  `}
 `
 
 export default function() {
   return (
     <Section>
-      Gallery Section
+      <Container>
+        <Center>
+          <Heading>บรรยากาศปีที่ผ่านมา</Heading>
+        </Center>
+        <br/>
+
+        <Grid>
+          {/* <Modal/>       */}
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+          <img width="100%" height="200px" src='https://r-cf.bstatic.com/images/hotel/max1024x768/116/116281465.jpg'></img>
+
+        </Grid>
+        <br/>
+      </Container>
     </Section>
   )
 }
